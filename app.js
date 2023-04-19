0591ae1cf29516ab18380a254a0cbae91a41ad21
radearsr <radea.surya4@gmail.com>
2023-04-19 16:49:52 +0700

    update merge scheduling with bot

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const Cron = require("croner");
 const { Telegraf } = require("telegraf");
 const checkerServices = require("./services/checker/checkerServices");
 const mysqlServices = require("./services/mysql/mysqlServices");
-const { formatDate } = require("./utils/DateService");
+const teleServices = require("./services/telegram/telegramService");
 const sslChecker = require("ssl-checker");
+const { formatDate } = require("./utils/DateService");
 
 const BOT_TOKEN = process.env.NODE_ENV === "production" ? process.env.BOT_TOKEN : process.env.BOT_TOKEN_DEV;   
 const WARN_DAYS = process.env.NODE_ENV === "production" ? 7 : 300;
@@ -93,6 +95,92 @@ bot.command("domain", async (ctx) => {
         break;
     }
   }
+});
+
+const monitoringSSLExpired = async () => {
+  /**
+   * * Read all data ssl domain from database
+  */
+  const resultSSLDomains = await mysqlServices.readAllSslDomain();
+  /**
+   * * Result variable is check remaining days all ssl domain with live checking
+  */
+  const liveSSLChecker = await Promise.all(resultSSLDomains.map(async (result) => {
+    const sslStatus = await checkerServices.getSSLStatus(result.domain, result.port);
+    const newExpired = formatDate(sslStatus.expired);
+    if (sslStatus.remaining <= WARN_DAYS) {
+      return {
+        nama: result.nama,
+        domain: result.domain,
+        remaining: sslStatus.remaining,
+        status: sslStatus.status,
+        expired: newExpired,
+        tempat: result.tempat,
+      };
+    }
+    return false;
+  }));
+  const filteredChecker = liveSSLChecker.filter((result) => result !== false);
+  /**
+   * * Filtering data from live checker, get data ssl when remaining < WARN_DAYS
+  */
+  if (filteredChecker.length >= 1) {
+    const filterWarningSSL = filteredChecker.filter((result) => result.remaining <= WARN_DAYS);
+    if (filterWarningSSL.length > 0) {
+      return await teleServices.sendWarningMessage(BOT_TOKEN, SEND_TO_ID, filterWarningSSL);
+    }
+  }
+};
+
+const monitoringDomainExpired = async () => {
+  /**
+   * * Read all data domain from database
+  */
+  const results = await mysqlServices.readAllDomain();
+  /**
+   * * Result variable is check remaining days all domain with live checking
+  */
+  const liveChecker = await Promise.all(results.map(async (result) => {
+    const today = new Date().getTime();
+    const checkDomain = await checkerServices.getInformationDomain(result.domain);
+    const dateOfDomain = new Date(checkDomain.expires_on).getTime();
+    const remainingTime = dateOfDomain - today;
+    const remainingDays = Math.round(remainingTime / (1000 * 60 * 60 * 24));
+    const newFormatDateDomain = formatDate(new Date(dateOfDomain).toISOString());
+    if (remainingDays <= WARN_DAYS) {
+      return {
+        hosting: result.hosting,
+        remaining: remainingDays,
+        domain: result.domain,
+        expired: newFormatDateDomain,
+      }
+    }
+    return false;
+  }));
+  const filteredChecker = liveChecker.filter((result) => result !== false);
+  /**
+   * * Filtering data from live checker, get data domain when remaining < WARN_DAYS
+  */
+  if (filteredChecker.length >= 1) {
+    const filteredData = liveChecker.filter((result) => result.remaining <= WARN_DAYS);
+    if (filteredData.length > 0) {
+      await teleServices.sendWarningDomainMessage(BOT_TOKEN, SEND_TO_ID, filteredData);
+    }
+  }
+};
+
+bot.command("cekdomain", () => {
+  monitoringDomainExpired();
+});
+
+bot.command("cekssl", () => {
+  monitoringSSLExpired();
+});
+
+Cron("0 0 7 * * *", { timezone: "Asia/Jakarta" }, async () => {
+  await teleServices.sendSelfAlert(BOT_TOKEN, process.env.ID_MY, "Cron Running Gaiss...");
+  monitoringSSLExpired();
+  monitoringDomainExpired();
 });
 
 bot.launch();
